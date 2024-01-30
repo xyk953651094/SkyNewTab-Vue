9c6684922a3ec713610d2c54a6fdcbf4f685055f
待看云开 <xyk953651094@qq.com>
2024-01-30 16:16:52 +0800

    修复已知问题

--- src/components/focusComponent.vue
+++ src/components/focusComponent.vue
@@ -215,13 +215,13 @@ watch(() => props.preferenceData.simpleMode, (newValue, oldValue) => {
 
 function setExtensionStorage(key, value) {
     const browserType = getBrowserType();
-    console.log(browserType + " " + key + " " + value);
-    // if (["Chrome", "Edge"].indexOf(browserType) !== -1) {
-    //     chrome.storage.local.set({[key]: value});
-    // }
-    // else if (["Firefox", "Safari"].indexOf(browserType) !== -1) {
-    //     browser.storage.local.set({[key]: value});
-    // }
+    // console.log(browserType + " " + key + " " + value);
+    if (["Chrome", "Edge"].indexOf(browserType) !== -1) {
+        chrome.storage.local.set({[key]: value});
+    }
+    else if (["Firefox", "Safari"].indexOf(browserType) !== -1) {
+        browser.storage.local.set({[key]: value});
+    }
 }
 
 function focusModeSwitchOnChange(checked) {
